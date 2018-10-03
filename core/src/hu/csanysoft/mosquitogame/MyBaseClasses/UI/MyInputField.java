04f24fe12f5bf0eb163c8b62cd2d5f6525e8dec9
Dávid Mátyás <davimatyi@outlook.com>
2018-10-03 20:11:46 +0200

    input mező beviteli ablakot dob csak ronda

--- core/src/hu/csanysoft/mosquitogame/MyBaseClasses/UI/MyInputField.java
+++ core/src/hu/csanysoft/mosquitogame/MyBaseClasses/UI/MyInputField.java
@@ -1,19 +1,29 @@
 package hu.csanysoft.mosquitogame.MyBaseClasses.UI;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import hu.csanysoft.mosquitogame.TheMosquitoGame;
 
-public class MyInputField extends MyTextField implements Input.TextInputListener {
+public class MyInputField extends MyTextField {
 
     TextFieldStyle validStyle, invalidStyle;
 
-    public MyInputField(String placeholder, TextFieldStyle validStyle, TextFieldStyle invalidStyle) {
+    public MyInputField(final String placeholder, TextFieldStyle validStyle, TextFieldStyle invalidStyle) {
         super(placeholder, validStyle);
         this.validStyle = validStyle;
         this.invalidStyle = invalidStyle;
         setTextFieldFilter(new TheMosquitoGame.FloatNumberFilter());
+        addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                Gdx.input.getTextInput(textListener, placeholder+": ", "0", "");
+            }
+        });
     }
 
     @Override
@@ -21,15 +31,7 @@ public class MyInputField extends MyTextField implements Input.TextInputListener
         super.init();
     }
 
-    @Override
-    public void input(String text) {
-        System.out.println(this.getText());
-    }
 
-    @Override
-    public void canceled() {
-        System.out.println(this.getText());
-    }
 
     public void setDataValidity(boolean valid){
         this.setStyle(valid ? validStyle : invalidStyle);
@@ -38,4 +40,16 @@ public class MyInputField extends MyTextField implements Input.TextInputListener
     void keyTyped(MyInputField myInputField,char c){
 
     }
+
+    Input.TextInputListener textListener = new Input.TextInputListener() {
+        @Override
+        public void input(String text) {
+            System.out.println(text);
+        }
+
+        @Override
+        public void canceled() {
+            System.out.println("cancelled ");
+        }
+    };
 }
